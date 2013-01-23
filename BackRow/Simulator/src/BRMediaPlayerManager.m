ec4a0c298ad8f996ad7ebad33e0796bc4d61190d
Eloy Durán <eloy.de.enige@gmail.com>
2013-01-23 20:37:20 +0100

    Let the BRMediaAsset know it hasBeenPlayed.

--- BackRow/Simulator/src/BRMediaPlayerManager.m
+++ BackRow/Simulator/src/BRMediaPlayerManager.m
@@ -1,6 +1,10 @@
 #import "BRMediaPlayerManager.h"
 #import <MediaPlayer/MediaPlayer.h>
 
+@interface BRMediaPlayerManager ()
+@property (retain) id<BRMediaAsset> currentAsset;
+@end
+
 @implementation BRMediaPlayerManager
 
 + (BRMediaPlayerManager *)singleton;
@@ -13,14 +17,38 @@
   return managerInstance;
 }
 
+- (id)init;
+{
+  if ((self = [super init])) {
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(moviePlayerStateChanged:)
+                                                 name:MPMoviePlayerPlaybackStateDidChangeNotification
+                                               object:nil];
+  }
+  return self;
+}
+
 - (void)presentMediaAsset:(id<BRMediaAsset>)asset options:(id)options;
 {
+  // TODO would be nice to release it when the view controller closes
+  self.currentAsset = asset;
+
   NSURL *URL = [NSURL URLWithString:[asset mediaURL]];
   MPMoviePlayerViewController *controller = [[MPMoviePlayerViewController alloc] initWithContentURL:URL];
   UIWindow *window = [[UIApplication sharedApplication] keyWindow];
   UINavigationController *stack = (UINavigationController *)window.rootViewController;
   [stack pushViewController:controller animated:YES];
   [controller release];
+}
+
+- (void)moviePlayerStateChanged:(NSNotification *)notification;
+{
+  if (self.currentAsset) {
+    MPMoviePlayerController *player = (MPMoviePlayerController *)notification.object;
+    if (player.playbackState == MPMoviePlaybackStatePlaying) {
+      self.currentAsset.hasBeenPlayed = YES;
+    }
+  }
 }
 
 @end
