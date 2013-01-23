2cdeec1958cdd2d7ca33351f8f37d4aaeeadbaec
Eloy Durán <eloy.de.enige@gmail.com>
2013-01-23 19:52:17 +0100

    Retrieve images by name from the BackRow resources bundle.

--- BackRow/Simulator/include/BRImage.h
+++ BackRow/Simulator/include/BRImage.h
@@ -3,3 +3,8 @@
 @interface BRImage : UIImage
 
 @end
+
+// Private implementation
+@interface UIImage (BackRow)
++ (UIImage *)backRowImageNamed:(NSString *)name;
+@end
--- BackRow/Simulator/src/BRImage.m
+++ BackRow/Simulator/src/BRImage.m
@@ -3,3 +3,21 @@
 @implementation BRImage
 
 @end
+
+@implementation UIImage (BackRow)
+
++ (UIImage *)backRowImageNamed:(NSString *)name;
+{
+  NSBundle *simBundle = [NSBundle mainBundle];
+  NSBundle *backRowBundle = [NSBundle bundleWithURL:[simBundle URLForResource:@"BackRow"
+                                                                withExtension:@"bundle"
+                                                                 subdirectory:nil]];
+  NSString *imagePath = [backRowBundle pathForResource:name ofType:@"png"];
+  if (imagePath) {
+    return [UIImage imageWithContentsOfFile:imagePath];
+  } else {
+    return nil;
+  }
+}
+
+@end
