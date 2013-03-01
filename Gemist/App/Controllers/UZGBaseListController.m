cc4c40195fb4b054de8aef6d995a96d7b9ddd8a3
Eloy Durán <eloy.de.enige@gmail.com>
2013-03-01 23:43:07 +0100

    Make UZGBaseListController selection wrap around at top and bottom.

--- Gemist/App/Controllers/UZGBaseListController.m
+++ Gemist/App/Controllers/UZGBaseListController.m
@@ -186,6 +186,29 @@
   }
 }
 
+- (BOOL)brEventAction:(BREvent *)event;
+{
+  if (event.value == 1) {
+    BOOL up = event.remoteAction == BREventUpButtonAction;
+    BOOL down = event.remoteAction == BREventDownButtonAction;
+    if (up || down) {
+      NSInteger max = self.itemCount-1;
+      if (max > 0) {
+        // Wrap to bottom of list.
+        if (up && self.list.selection == 0) {
+          self.list.selection = max;
+          return YES;
+        // Wrap to top of list.
+        } else if (down && self.list.selection == max) {
+          self.list.selection = 0;
+          return YES;
+        }
+      }
+    }
+  }
+  return [super brEventAction:event];
+}
+
 #pragma mark - UZGPagesListControllerDelegate
 
 - (void)pagesListController:(UZGPagesListController *)controller didSelectPage:(NSUInteger)page;
