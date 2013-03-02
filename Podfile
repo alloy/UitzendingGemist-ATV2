484686f0c6e8e2aa0373c6574a673ee474d76481
Eloy Durán <eloy.de.enige@gmail.com>
2013-03-02 01:10:03 +0100

    Add HockeySDK.

--- Podfile
+++ Podfile
@@ -2,6 +2,8 @@ platform :ios, "5.1"
 
 link_with ['Gemist', 'Gemist-Sim']
 
+pod "HockeySDK"
+
 pod "AFNetworking"
 pod "AFHTTPRequestOperationLogger"
 pod "Objective-C-HMTL-Parser"
---
+++ Pods/Pods-header.h
@@ -0,0 +1,7 @@
+// WARNING: This feature of CocoaPods is present for discussion purposes and might be discontinued or changed in future
+#define __COCOA_PODS
+
+#define __POD_AFHTTPRequestOperationLogger
+#define __POD_AFNetworking
+#define __POD_HockeySDK
+#define __POD_Objective_C_HMTL_Parser
